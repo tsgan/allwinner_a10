c03a9cd510137fb677e31c8ee83ed5121f75d1b3
tsgan <ganbold@gmail.com>
2012-12-19 15:20:29 +0800

    Initial commit

---
+++ aintc.c
@@ -0,0 +1,278 @@
+/*-
+ * Copyright (c) 2012 Ganbold Tsagaankhuu <ganbold@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/bus.h>
+#include <sys/kernel.h>
+#include <sys/ktr.h>
+#include <sys/module.h>
+#include <sys/rman.h>
+#include <machine/bus.h>
+#include <machine/intr.h>
+
+#include <dev/fdt/fdt_common.h>
+#include <dev/ofw/openfirm.h>
+#include <dev/ofw/ofw_bus.h>
+#include <dev/ofw/ofw_bus_subr.h>
+
+/* SW_VA_INT_IO_BASE                 0xe1c20400 */
+
+/**
+ * Interrupt controller registers
+ *
+ */
+#define SW_INT_VECTOR_REG                 0x00
+#define SW_INT_BASE_ADR_REG               0x04
+#define SW_INT_PROTECTION_REG             0x08
+#define SW_INT_NMI_CTRL_REG               0x0c
+#define SW_INT_IRQ_PENDING_REG0           0x10
+#define SW_INT_IRQ_PENDING_REG1           0x14
+#define SW_INT_IRQ_PENDING_REG2           0x18
+
+#define SW_INT_FIQ_PENDING_REG0           0x20
+#define SW_INT_FIQ_PENDING_REG1           0x24
+#define SW_INT_FIQ_PENDING_REG2           0x28
+
+#define SW_INT_SELECT_REG0                0x30
+#define SW_INT_SELECT_REG1                0x34
+#define SW_INT_SELECT_REG2                0x38
+
+#define SW_INT_ENABLE_REG0                0x40
+#define SW_INT_ENABLE_REG1                0x44
+#define SW_INT_ENABLE_REG2                0x48
+
+#define SW_INT_MASK_REG0                  0x50
+#define SW_INT_MASK_REG1                  0x54
+#define SW_INT_MASK_REG2                  0x58
+
+#define SW_INT_IRQNO_ENMI                 0
+#define SW_INT_END		          95
+
+struct a10_aintc_softc {
+	device_t		sc_dev;
+	struct resource *	aintc_res[3];
+	bus_space_tag_t		aintc_bst;
+	bus_space_handle_t	aintc_bsh;
+	uint8_t			ver;
+};
+
+static struct resource_spec a10_aintc_spec[] = {
+	{ SYS_RES_MEMORY,	0,	RF_ACTIVE },
+	{ -1, 0 }
+};
+
+
+static struct a10_aintc_softc *a10_aintc_sc = NULL;
+
+#define	aintc_read_4(reg)		\
+    bus_space_read_4(a10_aintc_sc->aintc_bst, a10_aintc_sc->aintc_bsh, reg)
+#define	aintc_write_4(reg, val)		\
+    bus_space_write_4(a10_aintc_sc->aintc_bst, a10_aintc_sc->aintc_bsh, reg, val)
+
+
+static int
+a10_aintc_probe(device_t dev)
+{
+	if (!ofw_bus_is_compatible(dev, "a10,aintc"))
+		return (ENXIO);
+	device_set_desc(dev, "A10 AINTC Interrupt Controller");
+	return (BUS_PROBE_DEFAULT);
+}
+
+static int
+a10_aintc_attach(device_t dev)
+{
+	struct a10_aintc_softc *sc = device_get_softc(dev);
+
+	sc->sc_dev = dev;
+
+	if (a10_aintc_sc)
+		return (ENXIO);
+
+	if (bus_alloc_resources(dev, a10_aintc_spec, sc->aintc_res)) {
+		device_printf(dev, "could not allocate resources\n");
+		return (ENXIO);
+	}
+
+	sc->aintc_bst = rman_get_bustag(sc->aintc_res[0]);
+	sc->aintc_bsh = rman_get_bushandle(sc->aintc_res[0]);
+
+	a10_aintc_sc = sc;
+
+	/* Disable & clear all interrupts */
+	aintc_write_4(SW_INT_ENABLE_REG0, 0);
+	aintc_write_4(SW_INT_ENABLE_REG1, 0);
+	aintc_write_4(SW_INT_ENABLE_REG2, 0);
+
+	aintc_write_4(SW_INT_MASK_REG0, 0xffffffff);
+	aintc_write_4(SW_INT_MASK_REG1, 0xffffffff);
+	aintc_write_4(SW_INT_MASK_REG2, 0xffffffff);
+
+	aintc_write_4(SW_INT_IRQ_PENDING_REG0, 0xffffffff);
+	aintc_write_4(SW_INT_IRQ_PENDING_REG1, 0xffffffff);
+	aintc_write_4(SW_INT_IRQ_PENDING_REG2, 0xffffffff);
+	aintc_write_4(SW_INT_FIQ_PENDING_REG0, 0xffffffff);
+	aintc_write_4(SW_INT_FIQ_PENDING_REG1, 0xffffffff);
+	aintc_write_4(SW_INT_FIQ_PENDING_REG2, 0xffffffff);
+
+	/*enable protection mode*/
+	aintc_write_4(SW_INT_PROTECTION_REG, 0x01);
+
+	/*config the external interrupt source type*/
+	aintc_write_4(SW_INT_NMI_CTRL_REG, 0x00);
+
+	aintc_write_4(SW_INT_VECTOR_REG, 0x01);
+
+	return (0);
+}
+
+static device_method_t a10_aintc_methods[] = {
+	DEVMETHOD(device_probe,		a10_aintc_probe),
+	DEVMETHOD(device_attach,	a10_aintc_attach),
+	{ 0, 0 }
+};
+
+static driver_t a10_aintc_driver = {
+	"aintc",
+	a10_aintc_methods,
+	sizeof(struct a10_aintc_softc),
+};
+
+static devclass_t a10_aintc_devclass;
+
+DRIVER_MODULE(aintc, simplebus, a10_aintc_driver, a10_aintc_devclass, 0, 0);
+
+int
+arm_get_next_irq(int last_irq)
+{
+        uint32_t value;
+        int i;
+
+        value = aintc_read_4(SW_INT_ENABLE_REG0);
+        for (i = 0; i < 32; i++) {
+                if (value & (1 << i))
+                        return (i);
+        }
+
+        value = aintc_read_4(SW_INT_ENABLE_REG1);
+        for (i = 0; i < 32; i++) {
+                if (value & (1 << i))
+                        return (i + 32);
+        }
+
+        value = aintc_read_4(SW_INT_ENABLE_REG2);
+        for (i = 0; i < 32; i++) {
+                if (value & (1 << i))
+                        return (i + 64);
+        }
+
+        return (-1);
+}
+
+void
+arm_mask_irq(uintptr_t nb)
+{
+        uint32_t value;
+
+	if(nb < 32){
+
+                value = aintc_read_4(SW_INT_ENABLE_REG0);
+                value &= ~(1 << nb);
+                aintc_write_4(SW_INT_ENABLE_REG0, value);
+
+		value = aintc_read_4(SW_INT_MASK_REG0);
+		value |= (1 << nb);
+		aintc_write_4(SW_INT_MASK_REG0, value);
+
+	} else if(nb < 64){
+		nb -= 32;
+
+                value = aintc_read_4(SW_INT_ENABLE_REG1);
+                value &= ~(1 << nb);
+                aintc_write_4(SW_INT_ENABLE_REG1, value);
+
+		value = aintc_read_4(SW_INT_MASK_REG1);
+		value |= (1 << nb);
+		aintc_write_4(SW_INT_MASK_REG1, value);
+
+	} else if(nb < 96){
+		nb -= 64;
+
+                value = aintc_read_4(SW_INT_ENABLE_REG2);
+                value &= ~(1 << nb);
+                aintc_write_4(SW_INT_ENABLE_REG2, value);
+
+		value = aintc_read_4(SW_INT_MASK_REG2);
+		value |= (1 << nb);
+		aintc_write_4(SW_INT_MASK_REG2, value);
+	}
+}
+
+void
+arm_unmask_irq(uintptr_t nb)
+{
+	uint32_t value;
+
+	printf("*** AINT_UNMASK *** 1\n");
+	if(nb < 32){
+
+		value = aintc_read_4(SW_INT_ENABLE_REG0);
+		value |= (1 << nb);
+		aintc_write_4(SW_INT_ENABLE_REG0, value);
+
+		value = aintc_read_4(SW_INT_MASK_REG0);
+		value &= ~(1 << nb);
+		aintc_write_4(SW_INT_MASK_REG0, value);
+		if(nb == SW_INT_IRQNO_ENMI) /* must clear pending bit when enabled */
+			aintc_write_4(SW_INT_IRQ_PENDING_REG0, (1 << SW_INT_IRQNO_ENMI));
+
+	} else if(nb < 64){
+		nb -= 32;
+
+		value = aintc_read_4(SW_INT_ENABLE_REG1);
+		value |= (1 << nb);
+		aintc_write_4(SW_INT_ENABLE_REG1, value);
+
+		value = aintc_read_4(SW_INT_MASK_REG1);
+		value &= ~(1 << nb);
+		aintc_write_4(SW_INT_MASK_REG1, value);
+
+	} else if(nb < 96){
+		nb -= 64;
+
+		value = aintc_read_4(SW_INT_ENABLE_REG2);
+		value |= (1 << nb);
+		aintc_write_4(SW_INT_ENABLE_REG2, value);
+
+		value = aintc_read_4(SW_INT_MASK_REG2);
+		value &= ~(1 << nb);
+		aintc_write_4(SW_INT_MASK_REG2, value);
+	}
+}

2eb220b1c4438b0362dfe2effc50da811adb67d6
tsgan <ganbold@gmail.com>
2013-08-02 11:16:27 +0800

    add cpu config module, update timer code

--- a20/timer_combined.c
+++ a20/timer_combined.c
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2012-2013 Ganbold Tsagaankhuu <ganbold@gmail.com>
+ * Copyright (c) 2012 Ganbold Tsagaankhuu <ganbold@gmail.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,7 +25,7 @@
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD$");
+__FBSDID("$FreeBSD: head/sys/arm/allwinner/timer.c 247463 2013-02-28 13:46:03Z mav $");
 
 #include <sys/param.h>
 #include <sys/systm.h>
@@ -52,8 +52,6 @@ __FBSDID("$FreeBSD$");
 
 #include <sys/kdb.h>
 
-#include "a20_cpu_cfg.h"
-
 /**
  * Timer registers addr
  *
@@ -64,6 +62,12 @@ __FBSDID("$FreeBSD$");
 #define SW_TIMER0_INT_VALUE_REG	0x14
 #define SW_TIMER0_CUR_VALUE_REG	0x18
 
+#define SW_COUNTER64LO_REG	0xa4
+#define SW_COUNTER64HI_REG	0xa8
+#define CNT64_CTRL_REG		0xa0
+
+#define CNT64_RL_EN		0x02 /* read latch enable */
+
 #define TIMER_ENABLE		(1<<0)
 #define TIMER_AUTORELOAD	(1<<1)
 #define TIMER_OSC24M		(1<<2) /* oscillator = 24mhz */
@@ -71,7 +75,7 @@ __FBSDID("$FreeBSD$");
 
 #define SYS_TIMER_CLKSRC	24000000 /* clock source */
 
-struct a20_timer_softc {
+struct a10_timer_softc {
 	device_t 	sc_dev;
 	struct resource *res[2];
 	bus_space_tag_t sc_bst;
@@ -82,63 +86,78 @@ struct a20_timer_softc {
 	struct eventtimer et;
 };
 
-int a20_timer_get_timerfreq(struct a20_timer_softc *);
+int a10_timer_get_timerfreq(struct a10_timer_softc *);
 
 #define timer_read_4(sc, reg)	\
 	bus_space_read_4(sc->sc_bst, sc->sc_bsh, reg)
 #define timer_write_4(sc, reg, val)	\
 	bus_space_write_4(sc->sc_bst, sc->sc_bsh, reg, val)
 
-#define REG_READ(reg)                   (*(volatile uint32_t *)(reg))
-#define REG_WRITE(reg, val)             (*(volatile uint32_t *)(reg) = (val))
-
-static u_int	a20_timer_get_timecount(struct timecounter *);
-static int	a20_timer_timer_start(struct eventtimer *,
+static u_int	a10_timer_get_timecount(struct timecounter *);
+static int	a10_timer_timer_start(struct eventtimer *,
     sbintime_t first, sbintime_t period);
-static int	a20_timer_timer_stop(struct eventtimer *);
+static int	a10_timer_timer_stop(struct eventtimer *);
 
-static int a20_timer_initialized = 0;
-static int a20_timer_hardclock(void *);
-static int a20_timer_probe(device_t);
-static int a20_timer_attach(device_t);
+static uint64_t timer_read_counter64(void);
 
-static struct timecounter a20_timer_timecounter = {
-	.tc_name           = "a20_timer timer0",
-	.tc_get_timecount  = a20_timer_get_timecount,
+static int a10_timer_initialized = 0;
+static int a10_timer_hardclock(void *);
+static int a10_timer_probe(device_t);
+static int a10_timer_attach(device_t);
+
+static struct timecounter a10_timer_timecounter = {
+	.tc_name           = "a10_timer timer0",
+	.tc_get_timecount  = a10_timer_get_timecount,
 	.tc_counter_mask   = ~0u,
 	.tc_frequency      = 0,
 	.tc_quality        = 1000,
 };
 
-struct a20_timer_softc *a20_timer_sc = NULL;
+struct a10_timer_softc *a10_timer_sc = NULL;
 
-static struct resource_spec a20_timer_spec[] = {
+static struct resource_spec a10_timer_spec[] = {
 	{ SYS_RES_MEMORY,	0,	RF_ACTIVE },
 	{ SYS_RES_IRQ,		0,	RF_ACTIVE },
 	{ -1, 0 }
 };
 
+static uint64_t
+timer_read_counter64(void)
+{
+	uint32_t lo, hi;
+
+	/* Latch counter, wait for it to be ready to read. */
+	timer_write_4(a10_timer_sc, CNT64_CTRL_REG, CNT64_RL_EN);
+	while (timer_read_4(a10_timer_sc, CNT64_CTRL_REG) & CNT64_RL_EN)
+		continue;
+
+	hi = timer_read_4(a10_timer_sc, SW_COUNTER64HI_REG);
+	lo = timer_read_4(a10_timer_sc, SW_COUNTER64LO_REG);
+
+	return (((uint64_t)hi << 32) | lo);
+}
+
 static int
-a20_timer_probe(device_t dev)
+a10_timer_probe(device_t dev)
 {
 
-	if (!ofw_bus_is_compatible(dev, "allwinner,sun7i-timer"))
+	if (!ofw_bus_is_compatible(dev, "allwinner,sun4i-timer"))
 		return (ENXIO);
 
-	device_set_desc(dev, "Allwinner A20 timer");
+	device_set_desc(dev, "Allwinner A10 timer");
 	return (BUS_PROBE_DEFAULT);
 }
 
 static int
-a20_timer_attach(device_t dev)
+a10_timer_attach(device_t dev)
 {
-	struct a20_timer_softc *sc;
+	struct a10_timer_softc *sc;
 	int err;
 	uint32_t val;
 
 	sc = device_get_softc(dev);
 
-	if (bus_alloc_resources(dev, a20_timer_spec, sc->res)) {
+	if (bus_alloc_resources(dev, a10_timer_spec, sc->res)) {
 		device_printf(dev, "could not allocate resources\n");
 		return (ENXIO);
 	}
@@ -148,10 +167,10 @@ a20_timer_attach(device_t dev)
 	sc->sc_bsh = rman_get_bushandle(sc->res[0]);
 
 	/* Setup and enable the timer interrupt */
-	err = bus_setup_intr(dev, sc->res[1], INTR_TYPE_CLK, a20_timer_hardclock,
+	err = bus_setup_intr(dev, sc->res[1], INTR_TYPE_CLK, a10_timer_hardclock,
 	    NULL, sc, &sc->sc_ih);
 	if (err != 0) {
-		bus_release_resources(dev, a20_timer_spec, sc->res);
+		bus_release_resources(dev, a10_timer_spec, sc->res);
 		device_printf(dev, "Unable to setup the clock irq handler, "
 		    "err = %d\n", err);
 		return (ENXIO);
@@ -171,21 +190,21 @@ a20_timer_attach(device_t dev)
 
 	/* Set desired frequency in event timer and timecounter */
 	sc->et.et_frequency = sc->timer0_freq;
-	sc->et.et_name = "a20_timer Eventtimer";
+	sc->et.et_name = "a10_timer Eventtimer";
 	sc->et.et_flags = ET_FLAGS_ONESHOT | ET_FLAGS_PERIODIC;
 	sc->et.et_quality = 1000;
 	sc->et.et_min_period = (0x00000005LLU << 32) / sc->et.et_frequency;
 	sc->et.et_max_period = (0xfffffffeLLU << 32) / sc->et.et_frequency;
-	sc->et.et_start = a20_timer_timer_start;
-	sc->et.et_stop = a20_timer_timer_stop;
+	sc->et.et_start = a10_timer_timer_start;
+	sc->et.et_stop = a10_timer_timer_stop;
 	sc->et.et_priv = sc;
 	et_register(&sc->et);
 
 	if (device_get_unit(dev) == 0)
-		a20_timer_sc = sc;
+		a10_timer_sc = sc;
 
-	a20_timer_timecounter.tc_frequency = sc->timer0_freq;
-	tc_init(&a20_timer_timecounter);
+	a10_timer_timecounter.tc_frequency = sc->timer0_freq;
+	tc_init(&a10_timer_timecounter);
 
 	if (bootverbose) {
 		device_printf(sc->sc_dev, "clock: hz=%d stathz = %d\n", hz, stathz);
@@ -193,23 +212,23 @@ a20_timer_attach(device_t dev)
 		device_printf(sc->sc_dev, "event timer clock frequency %u\n", 
 		    sc->timer0_freq);
 		device_printf(sc->sc_dev, "timecounter clock frequency %lld\n", 
-		    a20_timer_timecounter.tc_frequency);
+		    a10_timer_timecounter.tc_frequency);
 	}
 
-	a20_timer_initialized = 1;
+	a10_timer_initialized = 1;
 
 	return (0);
 }
 
 static int
-a20_timer_timer_start(struct eventtimer *et, sbintime_t first,
+a10_timer_timer_start(struct eventtimer *et, sbintime_t first,
     sbintime_t period)
 {
-	struct a20_timer_softc *sc;
+	struct a10_timer_softc *sc;
 	uint32_t count;
 	uint32_t val;
 
-	sc = (struct a20_timer_softc *)et->et_priv;
+	sc = (struct a10_timer_softc *)et->et_priv;
 
 	if (period != 0)
 		sc->sc_period = ((uint32_t)et->et_frequency * period) >> 32;
@@ -240,12 +259,12 @@ a20_timer_timer_start(struct eventtimer *et, sbintime_t first,
 }
 
 static int
-a20_timer_timer_stop(struct eventtimer *et)
+a10_timer_timer_stop(struct eventtimer *et)
 {
-	struct a20_timer_softc *sc;
+	struct a10_timer_softc *sc;
 	uint32_t val;
 
-	sc = (struct a20_timer_softc *)et->et_priv;
+	sc = (struct a10_timer_softc *)et->et_priv;
 
 	/* Disable timer0 */
 	val = timer_read_4(sc, SW_TIMER0_CTRL_REG);
@@ -258,7 +277,7 @@ a20_timer_timer_stop(struct eventtimer *et)
 }
 
 int
-a20_timer_get_timerfreq(struct a20_timer_softc *sc)
+a10_timer_get_timerfreq(struct a10_timer_softc *sc)
 {
 	return (sc->timer0_freq);
 }
@@ -270,12 +289,12 @@ cpu_initclocks(void)
 }
 
 static int
-a20_timer_hardclock(void *arg)
+a10_timer_hardclock(void *arg)
 {
-	struct a20_timer_softc *sc;
+	struct a10_timer_softc *sc;
 	uint32_t val;
 
-	sc = (struct a20_timer_softc *)arg;
+	sc = (struct a10_timer_softc *)arg;
 
 	/* Clear interrupt pending bit. */
 	timer_write_4(sc, SW_TIMER_IRQ_STA_REG, 0x1);
@@ -303,31 +322,35 @@ a20_timer_hardclock(void *arg)
 }
 
 u_int
-a20_timer_get_timecount(struct timecounter *tc)
+a10_timer_get_timecount(struct timecounter *tc)
 {
 
-	if (a20_timer_sc == NULL)
+	if (a10_timer_sc == NULL)
 		return (0);
 
+#ifdef _A20_CCM_H_
 	return ((u_int)a20_read_counter64());
+#else
+	return ((u_int)timer_read_counter64());
+#endif
 }
 
-static device_method_t a20_timer_methods[] = {
-	DEVMETHOD(device_probe,		a20_timer_probe),
-	DEVMETHOD(device_attach,	a20_timer_attach),
+static device_method_t a10_timer_methods[] = {
+	DEVMETHOD(device_probe,		a10_timer_probe),
+	DEVMETHOD(device_attach,	a10_timer_attach),
 
 	DEVMETHOD_END
 };
 
-static driver_t a20_timer_driver = {
-	"a20_timer",
-	a20_timer_methods,
-	sizeof(struct a20_timer_softc),
+static driver_t a10_timer_driver = {
+	"a10_timer",
+	a10_timer_methods,
+	sizeof(struct a10_timer_softc),
 };
 
-static devclass_t a20_timer_devclass;
+static devclass_t a10_timer_devclass;
 
-DRIVER_MODULE(a20_timer, simplebus, a20_timer_driver, a20_timer_devclass, 0, 0);
+DRIVER_MODULE(a10_timer, simplebus, a10_timer_driver, a10_timer_devclass, 0, 0);
 
 void
 DELAY(int usec)
@@ -335,16 +358,26 @@ DELAY(int usec)
 	uint32_t counter;
 	uint64_t end, now;
 
-	if (!a20_timer_initialized) {
+	if (!a10_timer_initialized) {
 		for (; usec > 0; usec--)
 			for (counter = 50; counter > 0; counter--)
 				cpufunc_nullop();
 		return;
 	}
 
+#ifdef _A20_CCM_H_
 	now = a20_read_counter64();
-	end = now + (a20_timer_sc->timer0_freq / 1000000) * (usec + 1);
+#else
+	now = timer_read_counter64();
+#endif
+	end = now + (a10_timer_sc->timer0_freq / 1000000) * (usec + 1);
 
-	while (now < end)
+	while (now < end) {
+#ifdef _A20_CCM_H_
 		now = a20_read_counter64();
+#else
+		now = timer_read_counter64();
+#endif
+	}
 }
+

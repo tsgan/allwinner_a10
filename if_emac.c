45464c9f72aa3d374dbaa5ed99433c8d192b9251
tsgan <ganbold@gmail.com>
2013-11-07 18:39:26 +0800

    delete extra :

--- if_emac.c
+++ if_emac.c
@@ -175,7 +175,7 @@ emac_set_hwaddr(struct emac_softc *sc, uint8_t *hwaddr)
 
 	if (bootverbose) {
 		device_printf(sc->emac_dev,
-		    "MAC address %02x:%02x:%02x:%02x:%02x:%02x:\n",
+		    "MAC address %02x:%02x:%02x:%02x:%02x:%02x\n",
 		    hwaddr[0], hwaddr[1], hwaddr[2], 
 		    hwaddr[3], hwaddr[4], hwaddr[5]);
 	}

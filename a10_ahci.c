d1d6f165ebffc209e40a8e42c6145908f8c6cae7
tsgan <ganbold@gmail.com>
2014-05-05 11:58:27 +0800

    update ahci, add channel related codes, still not working

--- a10_ahci.c
+++ a10_ahci.c
@@ -112,7 +112,7 @@ struct a10_ahci_softc {
 #define GPIO_AHCI_PWR		40
 
 /* Per-port registers */
-#define AHCI_P_OFFSET(port) (0x80 * (port))
+#define AHCI_P_OFFSET(port)	(0x80 * (port))
 #define SW_AHCI_P_DMA(p)	(0x170 + AHCI_P_OFFSET(p))
 
 static int	a10_ahci_probe(device_t dev);
@@ -129,28 +129,7 @@ static int	a10_ahci_setup_intr(device_t dev, device_t child,
     driver_intr_t *function, void *argument, void **cookiep);
 static int	a10_ahci_teardown_intr(device_t dev, device_t child,
     struct resource *irq, void *cookie);
-/*
-static void
-a10_ahci_channel_start(device_t dev, device_t child)
-{
-	struct a10_ahci_softc *sc;
-	struct ata_channel *ch;
-	uint32_t dma_reg, dma;
 
-	printf("---------- dma_reg first ------------\n");
-	sc = device_get_softc(dev);
-	ch = device_get_softc(child);
-	dma_reg = SW_AHCI_P_DMA(ch->unit);
-
-	printf("---------- dma_reg %x ------------\n", dma_reg);
-
-	dma = AHCI_READ_4(sc, dma_reg);
-	dma &= ~0xff00;
-	dma |= 0x4400;
-	AHCI_WRITE_4(sc, dma_reg, dma);
-	printf("---------- dma_reg last ------------\n");
-}
-*/
 static int
 a10_ahci_probe(device_t dev)
 {
@@ -292,21 +271,6 @@ a10_ahci_attach(device_t dev)
 	/* Enable SATA Clock in SATA PLL */
 	a10_clk_sata_activate();
 
-	/* Attach channels */
-	for (i = 0; i < SATA_CHAN_NUM; i++) {
-		child = device_add_child(dev, "ahcich", -1);
-		if (!child) {
-			device_printf(dev, "cannot add channel %d.\n", i);
-			error = ENOMEM;
-			goto err;
-		}
-//		ch = device_get_softc(child);
-//		ch->unit = i;
-//		printf("---------- 14.1 ------------\n");
-//		a10_ahci_channel_start(dev, child);
-//		printf("---------- 14.2 ------------\n");
-	}
-
 	/* Init phy */
 	a10_ahci_phy_init(dev);
 
@@ -330,9 +294,18 @@ a10_ahci_attach(device_t dev)
 		goto err;
 	}
 
+	/* Attach channels */
+	for (i = 0; i < SATA_CHAN_NUM; i++) {
+		child = device_add_child(dev, "ata", -1);
+		if (!child) {
+			device_printf(dev, "cannot add channel %d.\n", i);
+			error = ENOMEM;
+			goto err;
+		}
+	}
+
 	bus_generic_attach(dev);
 	return (0);
-
 err:
 	a10_ahci_detach(dev);
 	return (error);
@@ -476,7 +449,111 @@ static driver_t a10_ahci_driver = {
 devclass_t a10_ahci_devclass;
 
 DRIVER_MODULE(a10_ahci, simplebus, a10_ahci_driver, a10_ahci_devclass, 0, 0);
+MODULE_VERSION(a10_ahci, 1);
+MODULE_DEPEND(a10_ahci, ata, 1, 1, 1);
 
-//MODULE_VERSION(a10_ahci, 1);
-//MODULE_DEPEND(a10_ahci, cam, 1, 1, 1);
+static void
+ahci_channel_start(device_t dev, struct ata_channel *ch)
+{
+        struct a10_ahci_softc *sc = device_get_softc(device_get_parent(dev));
+	uint32_t dma_reg, dma;
+
+	printf("---------- dma_reg first ------------\n");
+	dma_reg = SW_AHCI_P_DMA(ch->unit);
+
+	printf("---------- dma_reg %x ------------\n", dma_reg);
+
+	dma = AHCI_READ_4(sc, dma_reg);
+	dma &= ~0xff00;
+	dma |= 0x4400;
+	AHCI_WRITE_4(sc, dma_reg, dma);
+	printf("---------- dma_reg last ------------\n");
+}
+
+static int
+ahci_channel_probe(device_t dev)
+{
+        struct ata_channel *ch = device_get_softc(dev);
+
+        ch->unit = 0;
+        ch->flags |= ATA_USE_16BIT | ATA_NO_SLAVE;
+        device_set_desc_copy(dev, "Allwinner Integrated SATA channel 0");
+
+        return ata_probe(dev);
+}
+
+static int
+ahci_channel_attach(device_t dev)
+{
+        struct a10_ahci_softc *sc = device_get_softc(device_get_parent(dev));
+        struct ata_channel *ch = device_get_softc(dev);
+        int i;
+
+        if (ch->attached)
+                return (0);
+
+        ch->dev = dev;
+//        ch->unit = device_get_unit(dev);
+        ch->flags |= ATA_USE_16BIT | ATA_NO_SLAVE | ATA_SATA;
+
+        for (i = 0; i < ATA_MAX_RES; i++)
+                ch->r_io[i].res = sc->sc_mem_res;
+
+        ch->r_io[ATA_ALTSTAT].res = sc->sc_mem_res;
+        ch->r_io[ATA_ALTSTAT].offset = ATA_ALTSTAT;
+        ch->r_io[ATA_CONTROL].res = sc->sc_mem_res;
+        ch->r_io[ATA_CONTROL].offset = ATA_CONTROL;
+
+        ch->r_io[ATA_DATA].offset = ATA_DATA;
+        ch->r_io[ATA_FEATURE].offset = ATA_FEATURE;
+        ch->r_io[ATA_COUNT].offset = ATA_COUNT;
+        ch->r_io[ATA_SECTOR].offset = ATA_SECTOR;
+        ch->r_io[ATA_CYL_LSB].offset = ATA_CYL_LSB;
+        ch->r_io[ATA_CYL_MSB].offset = ATA_CYL_MSB;
+        ch->r_io[ATA_DRIVE].offset = ATA_DRIVE;
+        ch->r_io[ATA_COMMAND].offset = ATA_COMMAND;
+        ch->r_io[ATA_ERROR].offset = ATA_FEATURE;
+        /* NB: should be used only for ATAPI devices */
+        ch->r_io[ATA_IREASON].offset = ATA_COUNT;
+        ch->r_io[ATA_STATUS].offset = ATA_COMMAND;
+
+        ch->r_io[ATA_IDX_ADDR].offset = 0;
+	ata_default_registers(dev);
+
+        /* Set SATA resources. */
+        ch->r_io[ATA_SSTATUS].res = sc->sc_mem_res;
+        ch->r_io[ATA_SSTATUS].offset = ATA_SSTATUS;
+        ch->r_io[ATA_SERROR].res = sc->sc_mem_res;
+        ch->r_io[ATA_SERROR].offset = ATA_SERROR;
+        ch->r_io[ATA_SCONTROL].res = sc->sc_mem_res;
+        ch->r_io[ATA_SCONTROL].offset = ATA_SCONTROL;
+
+	printf("---------- 14.1 ------------\n");
+	ahci_channel_start(dev, ch);
+	printf("---------- 14.2 ------------\n");
+
+        ata_generic_hw(dev);
+        ch->attached = 1;
+
+        return ata_attach(dev);
+}
+
+static device_method_t ahci_channel_methods[] = {
+        /* device interface */
+        DEVMETHOD(device_probe,     ahci_channel_probe),
+        DEVMETHOD(device_attach,    ahci_channel_attach),
+        DEVMETHOD(device_detach,    ata_detach),
+        DEVMETHOD(device_shutdown,  bus_generic_shutdown),
+        DEVMETHOD(device_suspend,   ata_suspend),
+        DEVMETHOD(device_resume,    ata_resume),
+
+        { 0, 0 }
+};
+
+driver_t ahci_channel_driver = {
+        "ata",
+        ahci_channel_methods,
+        sizeof(struct ata_channel),
+};
+DRIVER_MODULE(ata, a10_ahci, ahci_channel_driver, ata_devclass, 0, 0);
 

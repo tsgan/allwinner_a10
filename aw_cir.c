3dbafbe109cd41e7246613b637857a1b4ea9d9e3
tsgan <ganbold@gmail.com>
2016-10-22 19:42:43 +0800

    Remove packet decoding part and clean up the code a bit.

--- aw_cir.c
+++ aw_cir.c
@@ -50,98 +50,86 @@ __FBSDID("$FreeBSD$");
 #include <dev/evdev/input.h>
 #include <dev/evdev/evdev.h>
 
-#define	READ_1(_sc, _r)			bus_read_1((_sc)->res[0], (_r))
-#define	READ(_sc, _r)			bus_read_4((_sc)->res[0], (_r))
-#define	WRITE(_sc, _r, _v)		bus_write_4((_sc)->res[0], (_r), (_v))
+#define	READ(_sc, _r)		bus_read_4((_sc)->res[0], (_r))
+#define	WRITE(_sc, _r, _v)	bus_write_4((_sc)->res[0], (_r), (_v))
 
-/* IR Control Reg */
-#define	IR_CTL				0x00
-/* Global Enable bit */
-#define	 IR_CTL_GEN			(1 << 0)
-/* RX block enable bit */
-#define	 IR_CTL_RXEN			(1 << 1)
-/* CIR mode bits */
-#define	 IR_CTL_MD			(1 << 4) | (1 << 5)
+/* IR Control */
+#define	IR_CTL			0x00
+/* Global Enable */
+#define	 IR_CTL_GEN		(1 << 0)
+/* RX enable */
+#define	 IR_CTL_RXEN		(1 << 1)
+/* CIR mode eneble */
+#define	 IR_CTL_MD		(1 << 4) | (1 << 5)
 
 /* RX Config Reg */
-#define	IR_RXCTL			0x10
+#define	IR_RXCTL		0x10
 /* Pulse Polarity Invert flag */
-#define	 IR_RXCTL_RPPI			(1 << 2)
+#define	 IR_RXCTL_RPPI		(1 << 2)
 
-/* RX Data Reg */
-#define	IR_RXFIFO			0x20
+/* RX Data */
+#define	IR_RXFIFO		0x20
 
-/* RX Interrupt Control Reg */
-#define	IR_RXINT			0x2C
-/* RX FIFO Overflow bit */
-#define	 IR_RXINT_ROI_EN		(1 << 0)
-/* RX Packet End bit */
-#define	 IR_RXINT_RPEI_EN		(1 << 1)
-/* RX FIFO Data Available bit */
-#define	 IR_RXINT_RAI_EN		(1 << 4)
-/* RX FIFO available received byte level */
-#define	 IR_RXINT_RAL(val)		((val) << 8)
+/* RX Interrupt Control */
+#define	IR_RXINT		0x2C
+/* RX FIFO Overflow */
+#define	 IR_RXINT_ROI_EN	(1 << 0)
+/* RX Packet End */
+#define	 IR_RXINT_RPEI_EN	(1 << 1)
+/* RX FIFO Data Available */
+#define	 IR_RXINT_RAI_EN	(1 << 4)
+/* RX FIFO available byte level */
+#define	 IR_RXINT_RAL(val)	((val) << 8)
 
 /* RX Interrupt Status Reg */
-#define	IR_RXSTA			0x30
+#define	IR_RXSTA		0x30
 /* RX FIFO Get Available Counter */
-#define	 IR_RXSTA_COUNTER(val)		(((val) >> 8) & (sc->fifo_size * 2 - 1))
+#define	 IR_RXSTA_COUNTER(val)	(((val) >> 8) & (sc->fifo_size * 2 - 1))
 /* Clear all interrupt status */
-#define	 IR_RXSTA_CLEARALL		0xff
+#define	 IR_RXSTA_CLEARALL	0xff
+
+/* Frequency of Sample Clock = 46875.Hz, Cycle is 21.3us */
 
 /* IR Sample Configure Reg */
-#define	IR_CIR				0x34
-/* Filter Threshold = 8*42.7 = ~341us	< 500us */
-#define	 IR_RXFILT_VAL			(((8) & 0x3f) << 2)
-/* Idle Threshold = (2+1)*128*42.7 = ~16.4ms > 9ms */
-#define	 IR_RXIDLE_VAL			(((2) & 0xff) << 8)
+#define	IR_CIR			0x34
+/* Filter Threshold = 8 * 21.3 = ~128us < 200us */
+#define	 IR_RXFILT_VAL		(((8) & 0x3f) << 2)
+/* Idle Threshold = (2 + 1) * 128 * 42.7 = ~16.4ms > 9ms */
+#define	 IR_RXIDLE_VAL		(((2) & 0xff) << 8)
 
-/* Required frequency for IR0 or IR1 clock in CIR mode */
-#define	IR_BASE_CLK			8000000
-/* Frequency after IR internal divider  */
-#define	IR_CLK				(IR_BASE_CLK / 64)
-/* Sample period in ns */
-#define IR_SAMPLE			(1000000000ul / IR_CLK)
+/* Bit 15 - value (pulse/space) */
+#define	VAL_MASK		0x80
+/* Bits 0:14 - sample duration  */
+#define	PERIOD_MASK		0x7f
+/* From 0x80 to pulse bit */
+#define	PULSE_BIT		17
+#define	SAMPLES_TO_US(v)	((((unsigned long) v) * 1000000UL) / 46875UL)
+
+/* Clock rate for IR0 or IR1 clock in CIR mode */
+#define IR_BASE_CLK		8000000
+/* Freq sample = 3MHz/64 = 46875Hz (21.3us) */
+#define	IR_SAMPLE_64		(0 << 0)
+/* Freq sample 3MHz/128 = 23437.5Hz (42.7us) */
+#define	IR_SAMPLE_128		(1 << 0)
 /* Noise threshold in samples  */
-#define	IR_RXNOISE			1
+#define	IR_RXNOISE		1
 /* Idle threshold in samples */
-#define	IR_RXIDLE			20
+#define	IR_RXIDLE		20
 /* Time after which device stops sending data in ms */
-#define	IR_TIMEOUT			120
+#define	IR_TIMEOUT		120
 
-/* Active threshold */
-#define	IR_ACTIVE_T			((0 & 0xff) << 16)
-#define	IR_ACTIVE_T_C			((1 & 0xff) << 23)
-/* 80*42.7 = ~3.4ms, Lead1(4.5ms) > IR_L1_MIN */
-#define	IR_L1_MIN			(80)
-/* 40*42.7 = ~1.7ms, Lead0(4.5ms) Lead0R(2.25ms)> IR_L0_MIN */
-#define	IR_L0_MIN			(40)
-/* 26*42.7 = ~1109us ~= 561*2, Pluse < IR_PMAX */
-#define	IR_PMAX				(26)
-/* 26*42.7 = ~1109us ~= 561*2, D1 > IR_DMID, D0 =< IR_DMID */
-#define	IR_DMID				(26)
-/* 53*42.7 = ~2263us ~= 561*4, D < IR_DMAX */
-#define	IR_DMAX				(53)
+#define	A10_IR			1
+#define	A13_IR			2
 
-#define	IR_ERROR_CODE			(0xffffffff)
-#define	IR_REPEAT_CODE			(0x00000000)
-
-#define	A10_IR				1
-#define	A13_IR				2
+#define	IR_RAW_BUF_SIZE		128
 
 struct aw_ir_softc {
 	device_t		dev;
 	struct resource		*res[2];
 	void *			intrhand;
-	struct callout		co;
-	int			ticks;
 	int			fifo_size;
-	struct aw_ir_raw_buffer *rawbuf;
-	unsigned long		code;
-	unsigned long		dcnt;	/*Packet Count*/
-#define	IR_RAW_BUF_SIZE		128
+	unsigned int		dcnt;	/* Packet Count */
 	unsigned char		buf[IR_RAW_BUF_SIZE];
-	int			timer_used;
 	struct evdev_dev	*sc_evdev;
 };
 
@@ -158,197 +146,58 @@ static struct ofw_compat_data compat_data[] = {
 };
 
 static void
-aw_ir_reset_rawbuffer(struct aw_ir_softc *sc)
+aw_ir_buffer_reset(struct aw_ir_softc *sc)
 {
 
 	sc->dcnt = 0;
 }
 
 static void
-aw_ir_write_rawbuffer(struct aw_ir_softc *sc, unsigned char data)
+aw_ir_buffer_write(struct aw_ir_softc *sc, unsigned char data)
 {
 
 	if (sc->dcnt < IR_RAW_BUF_SIZE)
 		sc->buf[sc->dcnt++] = data;
 	else
-		device_printf(sc->dev, "aw_ir_write_rawbuffer: IR RX Buffer Full!\n");
-}
-
-static unsigned char
-aw_ir_read_rawbuffer(struct aw_ir_softc *sc)
-{
-	unsigned char data = 0x00;
-
-	if (sc->dcnt > 0)
-		data = sc->buf[--sc->dcnt];
-
-	return data;
+		device_printf(sc->dev, "IR RX Buffer Full!\n");
 }
 
 static int
-aw_ir_rawbuffer_empty(struct aw_ir_softc *sc)
-{
-
-	return (sc->dcnt == 0);
-}
-
-static int
-aw_ir_rawbuffer_full(struct aw_ir_softc *sc)
+aw_ir_buffer_full(struct aw_ir_softc *sc)
 {
 
 	return (sc->dcnt >= IR_RAW_BUF_SIZE);
 }
 
-static void
-print_err_code(struct aw_ir_softc *sc)
-{
-	unsigned long i = 0;
-
-	device_printf(sc->dev, "error code:\n");
-	for (i = 0; i < sc->dcnt; i++) {
-		device_printf(sc->dev, "%d:%d  ",
-		    ((sc->buf[i] & 0x80) >> 7), (sc->buf[i] & 0x7f));
-		if((i + 1) % 6 == 0)
-			device_printf(sc->dev, "\n");
-	}
-}
-
-static unsigned long
-aw_ir_packet_handler(struct aw_ir_softc *sc)
-{
-	unsigned long len;
-	unsigned char val = 0x00;
-	unsigned char last = 0x00;
-	unsigned long code = 0;
-	int bitCnt = 0;
-	unsigned long i=0;
-	unsigned int active_delay = 0;
-
-	device_printf(sc->dev, "dcnt = %d \n", (int)sc->dcnt);
-
-	/* Find Lead '1' */
-	active_delay = (IR_ACTIVE_T + 1) * (IR_ACTIVE_T_C ? 128 : 1);
-	device_printf(sc->dev, "%d active_delay = %d\n", __LINE__, active_delay);
-	len = 0;
-	len += (active_delay >> 1);
-	for (i = 0; i < sc->dcnt; i++) {
-		val = sc->buf[i];
-		if (val & 0x80)
-			len += val & 0x7f;
-		else {
-			if (len > IR_L1_MIN)
-				break;
-			len = 0;
-		}
-	}
-
-	device_printf(sc->dev, "%d len = %ld\n", __LINE__, len);
-
-	if ((val & 0x80) || (len <=IR_L1_MIN)){
-		device_printf(sc->dev, "start 1 error code\n" );
-		goto error_code; /* Invalid Code */
-	}
-
-	/* Find Lead '0' */
-	len = 0;
-	for (; i < sc->dcnt; i++) {
-		val = sc->buf[i];
-		if (val & 0x80) {
-			if(len > IR_L0_MIN)
-				break;
-			len = 0;
-		} else
-			len += val & 0x7f;
-	}
-
-	if ((!(val & 0x80)) || (len <= IR_L0_MIN)){
-		device_printf(sc->dev, "start 0 error code\n");
-		goto error_code; /* Invalid Code */
-	}
-
-	/* go decoding */
-	code = 0;  /* 0 for Repeat Code */
-	bitCnt = 0;
-	last = 1;
-	len = 0;
-	for (; i < sc->dcnt; i++) {
-		val = sc->buf[i];
-		if (last) {
-			if (val & 0x80)
-				len += val & 0x7f;
-			else {
-				if (len > IR_PMAX) {
-					/* Error Pulse */
-					device_printf(sc->dev,
-					    "len > IR_PMAX\n");
-					goto error_code;
-				}
-				last = 0;
-				len = val & 0x7f;
-			}
-		} else {
-			if (val & 0x80) {
-				if (len > IR_DMAX) {
-					/* Error Distant */
-					device_printf(sc->dev,
-					    "len > IR_DMAX\n");
-					goto error_code;
-				} else {
-					if (len > IR_DMID)  {
-						/* data '1'*/
-						code |= 1 << bitCnt;
-					}
-					bitCnt++;
-					if (bitCnt == 32)
-						break;  /* decode over */
-				}
-				last = 1;
-				len = val & 0x7f;
-			} else
-				len += val & 0x7f;
-		}
-	}
-	return (code);
-
-error_code:
-	print_err_code(sc);
-
-	return (IR_ERROR_CODE);
-}
-
-static int
-aw_ir_code_valid(unsigned long code)
-{
-	unsigned long v1, v2;
-
-	v1 = code & 0x00ff00ff;
-	v2 = (code & 0xff00ff00) >> 8;
-
-	return (((v1 ^ v2) & 0x00ff0000) == 0x00ff0000);
-}
-
 static unsigned char
-aw_ir_get_data(struct aw_ir_softc *sc)
+aw_ir_read_data(struct aw_ir_softc *sc)
 {
 
-    return (unsigned char)(READ_1(sc, IR_RXFIFO));
+    return (unsigned char)(READ(sc, IR_RXFIFO) & 0xff);
 }
 
-static void aw_ir_timer_handle(void *arg)
+static void
+aw_ir_handle_packets(struct aw_ir_softc *sc)
 {
-    struct aw_ir_softc *sc = arg;
+	unsigned int i;
+	unsigned int val = 0;
+	unsigned int pulse;
 
-    sc->timer_used = 0;
+	device_printf(sc->dev, "Buffer len: %d\n", sc->dcnt);
+	for(i = 0; i < sc->dcnt; ) {
+		pulse = (unsigned int) (sc->buf[i] & VAL_MASK);
+		for(; i < sc->dcnt && (sc->buf[i] & VAL_MASK) == pulse; i++) {
+			val += SAMPLES_TO_US(sc->buf[i] & PERIOD_MASK);
+		}
+		val |= pulse << PULSE_BIT;
+		device_printf(sc->dev,
+		    "buf: %x, val: %x, level: %d for %dus\n",
+		    sc->buf[i], val,
+		    (sc->buf[i] & VAL_MASK) ? 1 : 0, val & PERIOD_MASK);
 
-    /* Time Out, means that the key is up */
-    evdev_push_event(sc->sc_evdev, EV_KEY, (sc->code >> 16) & 0xff, 0);
-    evdev_sync(sc->sc_evdev);
-
-    device_printf(sc->dev, "IR KEY TIMER OUT UP\n");
-
-    sc->dcnt = 0;
-
-    device_printf(sc->dev, "aw_ir_timer_handle: timeout\n");
+		evdev_push_event(sc->sc_evdev, EV_KEY, val, 1);
+		evdev_sync(sc->sc_evdev);
+	}
 }
 
 static void
@@ -357,8 +206,6 @@ aw_ir_intr(void *arg)
 	struct aw_ir_softc *sc;
 	uint32_t val;
 	int i, dcnt;
-	unsigned long code;
-	int code_valid;
 
 	sc = (struct aw_ir_softc *)arg;
 
@@ -370,84 +217,30 @@ aw_ir_intr(void *arg)
 
 	/* When Rx FIFO Data available or Packet end */
 	if (val & (IR_RXINT_RAI_EN | IR_RXINT_RPEI_EN)) {
-		/* Get available message count in fifo */
+		/* Get available message count in RX FIFO */
 		dcnt  = IR_RXSTA_COUNTER(val);
-		/* When there is a data, read FIFO */
+		/* Read FIFO */
 		for (i = 0; i < dcnt; i++) {
-			if (aw_ir_rawbuffer_full(sc))
-				aw_ir_get_data(sc);
-			else
-				aw_ir_write_rawbuffer(sc, aw_ir_get_data(sc));
+			if (aw_ir_buffer_full(sc)) {
+				device_printf(sc->dev, "raw buffer full\n");
+				break;
+			} else
+				aw_ir_buffer_write(sc, aw_ir_read_data(sc));
 		}
 	}
 
-	if (val & IR_RXINT_ROI_EN) {
-		/* When there is RX FIFO overflow */
-		/* flush raw buffer */
-		aw_ir_reset_rawbuffer(sc);
-	} else if (val & IR_RXINT_RPEI_EN) {
-		/* When there is RX Packet end */
-
-		if (aw_ir_rawbuffer_full(sc)) {
-			device_printf(sc->dev, "Raw Buffer Full!\n");
-			sc->dcnt = 0;
-			return;
-		}
-
-		code = aw_ir_packet_handler(sc);
+	if (val & IR_RXINT_RPEI_EN) {
+		/* RX Packet end */
+		device_printf(sc->dev, "RX Packet end\n");
+		aw_ir_handle_packets(sc);
+		device_printf(sc->dev, "Buffer written\n");
 		sc->dcnt = 0;
-		code_valid = aw_ir_code_valid(code);
-
-		device_printf(sc->dev, "IR code = 0x%lx\n", code);
-
-		if (sc->timer_used) {
-			if (code_valid) {  /* the pre-key is released */
-				evdev_push_event(sc->sc_evdev,
-				    EV_KEY, (sc->code >> 16) & 0xff, 0);
-				evdev_sync(sc->sc_evdev);
-
-				device_printf(sc->dev, "IR KEY UP\n");
-
-				sc->dcnt = 0;
-			}
-			if ((code == IR_REPEAT_CODE) || (code_valid)) {
-				/* Error, may interfere from other sources */
-				callout_reset(&sc->co, sc->ticks,
-				    aw_ir_timer_handle, sc);
-				// mod_timer(s_timer, jiffies + (HZ / 5));
-			}
-		} else {
-			if (code_valid) {
-				callout_reset(&sc->co, sc->ticks,
-				    aw_ir_timer_handle, sc);
-				// mod_timer(s_timer, jiffies + (HZ / 5));
-				sc->timer_used = 1;
-			}
-		}
-
-		if (sc->timer_used) {
-			sc->dcnt++;
-			if (sc->dcnt == 1) {
-				if (code_valid) {
-					/* update saved code */
-					sc->code = code;
-				}
-				device_printf(sc->dev, "IR RAW CODE : %lu\n",
-				    (sc->code >> 16) & 0xff);
-				evdev_push_event(sc->sc_evdev,
-				    EV_KEY, (sc->code >> 16) & 0xff, 1);
-
-				device_printf(sc->dev, "IR CODE : %lu\n",
-				    (sc->code >> 16) & 0xff);
-				evdev_sync(sc->sc_evdev);
-
-				device_printf(sc->dev, "IR KEY VALE %lu\n",
-				    (sc->code >> 16) & 0xff);
-			}
-		}
-		device_printf(sc->dev,
-		    "RX Packet End, code=0x%x, ir_code=0x%x, timer_used=%d\n",
-		    (int)code, (int)sc->code, sc->timer_used);
+	}
+	if (val & IR_RXINT_ROI_EN) {
+		/* RX FIFO overflow */
+		device_printf(sc->dev, "RX FIFO overflow\n");
+		/* Flush raw buffer */
+		aw_ir_buffer_reset(sc);
 	}
 }
 
@@ -502,12 +295,8 @@ aw_ir_attach(device_t dev)
 		}
 	}
 
-	sc->dcnt = 0;
-	sc->code = 0;
-	sc->timer_used = 0;
-
 	/* Reset buffer */
-	aw_ir_reset_rawbuffer(sc);
+	aw_ir_buffer_reset(sc);
 
 	/* Get clocks and enable them */
 	err = clk_get_by_ofw_name(dev, 0, "apb", &clk_gate);
@@ -549,21 +338,25 @@ aw_ir_attach(device_t dev)
 	/* Enable CIR Mode */
 	WRITE(sc, IR_CTL, IR_CTL_MD);
 
-	/* Set noise and idle threshold */
-	WRITE(sc, IR_CIR, IR_RXFILT_VAL | IR_RXIDLE_VAL);
+	/* Set clock sample, filter, idle, active thresholds */
+	/* Fsample = 3MHz/64 =46875Hz (21.3us) */
+	val = IR_SAMPLE_64;
+	val |= (IR_RXFILT_VAL | IR_RXIDLE_VAL);
+	WRITE(sc, IR_CIR, val);
 
 	/* Invert Input Signal */
 	WRITE(sc, IR_RXCTL, IR_RXCTL_RPPI);
 
-	/* Clear All RX Interrupt Statuses */
+	/* Clear All RX Interrupt Status */
 	WRITE(sc, IR_RXSTA, IR_RXSTA_CLEARALL);
 
 	/*
 	 * Enable interrupt in case of overflow, packet end
-	 * and FIFO available with trigger level
+	 * and FIFO available with trigger level.
+	 * Rx FIFO Threshold = FIFOsz/2
 	 */
 	WRITE(sc, IR_RXINT, IR_RXINT_ROI_EN | IR_RXINT_RPEI_EN |
-	    IR_RXINT_RAI_EN | IR_RXINT_RAL(sc->fifo_size / 2 - 1));
+	    IR_RXINT_RAI_EN | IR_RXINT_RAL((sc->fifo_size >> 1) - 1));
 
 	/* Enable IR Module */
 	val = READ(sc, IR_CTL);
@@ -576,6 +369,7 @@ aw_ir_attach(device_t dev)
 	evdev_support_prop(sc->sc_evdev, INPUT_PROP_DIRECT);
 	evdev_support_event(sc->sc_evdev, EV_SYN);
 	evdev_support_event(sc->sc_evdev, EV_KEY);
+	evdev_support_event(sc->sc_evdev, EV_REP);
 	evdev_support_event(sc->sc_evdev, EV_MSC);
 
 	evdev_support_msc(sc->sc_evdev, MSC_SCAN);
@@ -586,12 +380,6 @@ aw_ir_attach(device_t dev)
 		    "failed to register evdev: error=%d\n", err);
 		goto error;
 	}
-	if (hz > 100)
-		sc->ticks = hz / 100;
-	else
-		sc->ticks = 1;
-
-	callout_init(&sc->co, 1);
 
 	return (0);
 error:
